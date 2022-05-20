350349e13fdc551e0be3c71b12dea73739089010
Jared Borders <jlbxsxs@gmail.com>
2022-05-20 14:44:21 -0500

    Implement position storage logic

--- contracts/MarginBase.sol
+++ contracts/MarginBase.sol
@@ -6,10 +6,24 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/IAddressResolver.sol";
 import "./interfaces/IFuturesMarket.sol";
 
+/// @TODO: Document
 contract MarginBase is MinimalProxyable {
+
+    //////////////////////////////////////
+    ///////////// CONSTANTS //////////////
+    //////////////////////////////////////
 
     // tracking code used when modifying positions
     bytes32 private constant TRACKING_CODE = "KWENTA";
+
+    //////////////////////////////////////
+    /////////////// TYPES ////////////////
+    //////////////////////////////////////
+
+    struct ActiveMarketPosition {
+        uint128 margin;
+        int128 size;
+    }
 
     //////////////////////////////////////
     /////////////// STATE ////////////////
@@ -21,28 +35,20 @@ contract MarginBase is MinimalProxyable {
     // token contract used for account margin
     IERC20 public marginAsset;
 
+    // number of active market positions account has
+    uint16 public numberOfActivePositions = 0;
+
+    // market keys that the account has active positions in
+    bytes32[] public activeMarketKeys;
+
     // market keys mapped to active market positions
-    mapping(bytes32 => MarketPosition) public activeMarketPositions;
-
-    //////////////////////////////////////
-    ////////// DATA-STRUCTURES ///////////
-    //////////////////////////////////////
-
-    struct MarketPosition {
-        uint size;
-        uint leverage;
-        bytes32 marketKey;
-    }
+    mapping(bytes32 => ActiveMarketPosition) public activeMarketPositions;
 
     //////////////////////////////////////
     ///////////// MODIFIERS //////////////
     //////////////////////////////////////
     
-    modifier validLeverage(int8 _leverage) {
-        require(_leverage <= 10, "Leverage must not exceed 10");
-        require(_leverage > 0, "Leverage must exceed 0");
-        _;
-    }
+    // @TODO: TBD
 
     //////////////////////////////////////
     //// CONSTRUCTOR / INITIALIZER ///////
@@ -64,66 +70,140 @@ contract MarginBase is MinimalProxyable {
     //////////////////////////////////////
 
     /// @notice deposit amount of marginAsset into account
-    function deposit(uint _amount) external onlyOwner {
+    function deposit(
+        uint _amount
+    ) external onlyOwner {
         marginAsset.transferFrom(owner(), address(this), _amount);
     }
 
     /// @notice withdraw amount of marginAsset from account
-    function withdraw(uint _amount) external onlyOwner {
+    function withdraw(
+        uint _amount
+    ) external onlyOwner {
         marginAsset.transfer(owner(), _amount);
     }
 
+    /// @TODO: Document
+    function closeMarketPosition(
+        bytes32 _marketKey
+    ) external onlyOwner {
+        // define market via _marketKey
+        IFuturesMarket market = futuresMarket(_marketKey);
+
+        // close market position with KWENTA tracking code
+        market.closePositionWithTracking(TRACKING_CODE);
+
+        /// @dev update state:
+        // delete formerly active position
+        removeActiveMarketPositon(_marketKey);
+    }
+
+    /// @TODO: Document
     function depositAndModifyPositionForMarket(
-        int256 _size,
-        bytes32 _marketKey,
-        int8 _leverage
-    ) external onlyOwner validLeverage(_leverage) {
+        int256 _depositSize,
+        int256 _sizeDelta,
+        bytes32 _marketKey
+    ) external onlyOwner {
         // define market via _marketKey
         IFuturesMarket market = futuresMarket(_marketKey);
 
         /// @notice alter the amount of margin in specific market position
         /// @dev positive input triggers a deposit; a negative one, a withdrawal
-        market.transferMargin(_size);
+        market.transferMargin(_depositSize);
 
         // modify position in specific market with KWENTA tracking code
-        // @TODO: how to define sizeDelta
-        int256 sizeDelta = _size; // * _leverage ????
-        market.modifyPositionWithTracking(sizeDelta, TRACKING_CODE);
+        market.modifyPositionWithTracking(_sizeDelta, TRACKING_CODE);
+
+        // fetch new position data from Synthetix
+        (,,uint128 margin,,int128 size) = market.positions(address(this));
+
+        // update state for given open market position
+        updateActiveMarketPosition(_marketKey, margin, size);
     }
 
+    /// @TODO: Document
     function modifyPositionForMarketAndWithdraw(
-        int256 _size,
-        bytes32 _marketKey,
-        int8 _leverage
+        int256 withdrawSize,
+        int256 _sizeDelta,
+        bytes32 _marketKey
     ) external onlyOwner {
         // define market via _marketKey
         IFuturesMarket market = futuresMarket(_marketKey);
 
         // modify position in specific market with KWENTA tracking code
-        // @TODO: how to define sizeDelta
-        int256 sizeDelta = _size; // * leverage ????
-        market.modifyPositionWithTracking(sizeDelta, TRACKING_CODE);
+        market.modifyPositionWithTracking(_sizeDelta, TRACKING_CODE);
 
         /// @notice alter the amount of margin in specific market position
         /// @dev positive input triggers a deposit; a negative one, a withdrawal
-        market.transferMargin(_size * -1);
+        market.transferMargin(withdrawSize);
+
+        // fetch new position data from Synthetix
+        (,,uint128 margin,,int128 size) = market.positions(address(this));
+
+        // update state for given open market position
+        updateActiveMarketPosition(_marketKey, margin, size);
     }
 
+    /// @TODO: Document
     function rebalance() external {
-        // @TODO: Rebalance margin in an equal manner
+        /// @TODO: Implement
+        /// @TODO: Design parameter describing how to rebalance
     }
-
 
     //////////////////////////////////////
     ///////// INTERNAL FUNCTIONS /////////
     //////////////////////////////////////
 
     /// @notice addressResolver fetches IFuturesMarket address for specific market
-    function futuresMarket(bytes32 _marketKey) internal view returns (IFuturesMarket) {
+    function futuresMarket(
+        bytes32 _marketKey
+    ) internal view returns (IFuturesMarket) {
         return IFuturesMarket(addressResolver.requireAndGetAddress(
             _marketKey, 
             "MarginBase: Could not get Futures Market"
         ));
+    }
+
+    /// @TODO: Document
+    function updateActiveMarketPosition(
+        bytes32 _marketKey,
+        uint128 _margin,
+        int128 _size
+    ) internal {
+        ActiveMarketPosition memory newPosition = ActiveMarketPosition(_margin, _size);
+
+        // check if this is updating a position or creating one
+        if (activeMarketPositions[_marketKey].margin == 0) {
+            activeMarketKeys.push(_marketKey);
+            numberOfActivePositions++;
+        }
+
+        // update state of active market positions
+        activeMarketPositions[_marketKey] = newPosition;
+    }
+
+    /// @TODO: Document
+    function removeActiveMarketPositon(
+        bytes32 _marketKey
+    ) internal {
+        delete activeMarketPositions[_marketKey];
+        numberOfActivePositions--;
+        
+        require(activeMarketKeys.length > 0, "MarginBase: Empty array");
+        bool found = false;
+
+        for (uint i = 0; i < activeMarketKeys.length; i++) {
+            // once `_marketKey` is encountered, swap with 
+            // last element in array exit for-loop
+            if (activeMarketKeys[i] == _marketKey) {
+                activeMarketKeys[i] = activeMarketKeys[activeMarketKeys.length - 1];
+                found = true;
+                break;
+            } 
+        }
+        // remove last element (which will be `_marketKey`)
+        require(found, "MarginBase: Market Key not found");
+        activeMarketKeys.pop();
     }
 
 }
