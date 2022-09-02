6bc480025d23c4d181bbb320b6733c6bb527801a
JChiaramonte7 <jeremychiaramonte@gmail.com>
2022-09-01 23:49:48 -0400

    increase integration coverage

--- contracts/utils/OpsReady.sol
+++ contracts/utils/OpsReady.sol
@@ -5,22 +5,7 @@ import {
     SafeERC20,
     IERC20
 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-
-interface IOps {
-    function gelato() external view returns (address payable);
-    
-    function createTaskNoPrepayment(
-        address _execAddress,
-        bytes4 _execSelector,
-        address _resolverAddress,
-        bytes calldata _resolverData,
-        address _feeToken
-    ) external returns (bytes32 task);
-
-    function cancelTask(bytes32 _taskId) external;
-
-    function getFeeDetails() external view returns (uint256, address);
-}
+import "../interfaces/IOps.sol";
 
 abstract contract OpsReady {
     address public ops;
