8c47edf7d0066fab152b475d1e7bf21d0fe95901
JChiaramonte7 <jeremychiaramonte@gmail.com>
2022-10-05 16:10:12 -0400

    make fee cap inclusive of max fee

--- contracts/MarginBase.sol
+++ contracts/MarginBase.sol
@@ -656,7 +656,7 @@ contract MarginBase is MinimalProxyable, IMarginBase, OpsReady {
                     SUSD,
                     futuresMarket(order.marketKey).baseAsset()
                 );
-            if (tooVolatile || dynamicFee >= order.maxDynamicFee) {
+            if (tooVolatile || dynamicFee > order.maxDynamicFee) {
                 return (false, 0);
             }
         }
