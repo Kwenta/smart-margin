7b916ad16d23fe628ac52028ad121ef0a5252f26
JaredBorders <jlbxsxs@gmail.com>
2022-12-07 16:36:28 -0500

    👷🏻‍♂️ Add Interfaces

---
+++ contracts/interfaces/IMarginBaseSettings.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.13;
+
+/// @title Kwenta MarginBaseSettings Interface
+/// @author JaredBorders (jaredborders@proton.me)
+interface IMarginBaseSettings {
+    function limitOrderFee() external view returns (uint256);
+
+    function setLimitOrderFee(uint256 _fee) external;
+
+    function setStopOrderFee(uint256 _fee) external;
+
+    function setTradeFee(uint256 _fee) external;
+
+    function setTreasury(address _treasury) external;
+
+    function stopOrderFee() external view returns (uint256);
+
+    function tradeFee() external view returns (uint256);
+
+    function treasury() external view returns (address);
+}
