35d454379cbe8623595743546671150ffaf7a6db
JaredBorders <jlbxsxs@gmail.com>
2022-12-07 16:34:54 -0500

    🏭 Update Factory

--- contracts/MarginAccountFactory.sol
+++ contracts/MarginAccountFactory.sol
@@ -1,19 +1,21 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.13;
 
-import "./utils/MinimalProxyFactory.sol";
-import "./MarginBase.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./interfaces/IMarginAccountFactory.sol";
+import "./interfaces/IMarginAccountFactoryStorage.sol";
+import "./MarginBase.sol";
+import "./utils/MinimalProxyFactory.sol";
 
 /// @title Kwenta MarginBase Factory
 /// @author JaredBorders (jaredborders@pm.me), JChiaramonte7 (jeremy@bytecode.llc)
 /// @notice Factory which enables deploying a MarginBase account for any user
-contract MarginAccountFactory is MinimalProxyFactory {
-    string public version; // format: (0.1.0)
+contract MarginAccountFactory is IMarginAccountFactory, MinimalProxyFactory {
+    string public constant VERSION = "2.0.0";
 
-    /*///////////////////////////////////////////////////////////////
-                                Immutables
-    ///////////////////////////////////////////////////////////////*/
+    /*//////////////////////////////////////////////////////////////
+                               IMMUTABLES
+    //////////////////////////////////////////////////////////////*/
 
     /// @notice MarginBase contract acting as user's account
     MarginBase public immutable implementation;
@@ -21,60 +23,91 @@ contract MarginAccountFactory is MinimalProxyFactory {
     /// @notice ERC20 token used to interact with markets
     IERC20 public immutable marginAsset;
 
+    /// @notice persistent storage for all accounts/factories v2.0.0 and later
+    IMarginAccountFactoryStorage public immutable store;
+
     /// @notice synthetix address resolver
     address public immutable addressResolver;
 
-    /// @notice settings for MarginBase accounts
+    /// @notice settings for accounts
     address public immutable marginBaseSettings;
-    
+
     /// @notice gelato ops
     address payable public immutable ops;
 
-    /*///////////////////////////////////////////////////////////////
-                                Events
-    ///////////////////////////////////////////////////////////////*/
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
 
-    event NewAccount(address indexed owner, address account);
+    /// @notice emitted when new account is created
+    /// @param owner: account creator
+    /// @param account: address of account that was created
+    event NewAccount(address indexed owner, address indexed account);
 
-    /*///////////////////////////////////////////////////////////////
-                                Constructor
-    ///////////////////////////////////////////////////////////////*/
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice thrown when newAccount() is called
+    /// by an address which has already made an account
+    /// @param account: address of account previously created
+    error AlreadyCreatedAccount(address account);
+
+    /*//////////////////////////////////////////////////////////////
+                              CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
 
     /// @notice deploy MarginBase implementation to later be cloned
-    /// @param _version: version of contract
+    /// @param _store: address of store for persistent account data
     /// @param _marginAsset: token contract address used for account margin
     /// @param _addressResolver: contract address for synthetix address resolver
     /// @param _marginBaseSettings: contract address for MarginBase account settings
     /// @param _ops: contract address for gelato ops -- must be payable
     constructor(
-        string memory _version,
+        address _store,
         address _marginAsset,
         address _addressResolver,
         address _marginBaseSettings,
         address payable _ops
     ) {
-        version = _version;
-        implementation = new MarginBase();
+        store = IMarginAccountFactoryStorage(_store);
         marginAsset = IERC20(_marginAsset);
         addressResolver = _addressResolver;
+
+        // deploy proxy logic
+        implementation = new MarginBase();
 
         /// @dev MarginBaseSettings must exist prior to MarginAccountFactory
         marginBaseSettings = _marginBaseSettings;
 
+        // assign Gelato ops
         ops = _ops;
     }
 
-    /*///////////////////////////////////////////////////////////////
-                            Account Deployment
-    ///////////////////////////////////////////////////////////////*/
+    /*//////////////////////////////////////////////////////////////
+                           ACCOUNT DEPLOYMENT
+    //////////////////////////////////////////////////////////////*/
 
     /// @notice clone MarginBase (i.e. create new account for user)
     /// @dev this contract is the initial owner of cloned MarginBase,
     /// but ownership is transferred after successful initialization
     function newAccount() external returns (address payable accountAddress) {
+        // ensure one account per address
+        if (store.deployedMarginAccounts(msg.sender) != address(0)) {
+            revert AlreadyCreatedAccount(
+                store.deployedMarginAccounts(msg.sender)
+            );
+        }
+
+        // create account
         accountAddress = payable(
             _cloneAsMinimalProxy(address(implementation), "Creation failure")
         );
+
+        // update store
+        store.addDeployedAccount(msg.sender, accountAddress);
+
+        // initialize new account
         MarginBase account = MarginBase(accountAddress);
         account.initialize(
             address(marginAsset),
@@ -82,6 +115,8 @@ contract MarginAccountFactory is MinimalProxyFactory {
             marginBaseSettings,
             ops
         );
+
+        // transfer ownership of account to caller
         account.transferOwnership(msg.sender);
 
         emit NewAccount(msg.sender, accountAddress);
