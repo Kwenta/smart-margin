b3d0b7a566086e51bb290c0fd176bab44dd7d50c
JaredBorders <jlbxsxs@gmail.com>
2022-12-16 12:52:20 -0500

    🦴 Scaffold Integration Tests

--- test/integration/account.behavior.t.sol
+++ test/integration/account.behavior.t.sol
@@ -95,11 +95,10 @@ contract AccountBehaviorTest is Test {
                             ACCOUNT CREATION
     //////////////////////////////////////////////////////////////*/
 
+    /// @notice create account via the MarginAccountFactory
     function testAccountCreated() external {
         // call factory to create account
-        MarginBase account = MarginBase(
-            payable(marginAccountFactory.newAccount())
-        );
+        MarginBase account = createAccount();
 
         // check account address exists
         assert(address(account) != address(0));
@@ -116,9 +115,138 @@ contract AccountBehaviorTest is Test {
         assert(address(account.marginAsset()) == address(sUSD));
         assert(address(account.owner()) == address(this));
         assert(address(account.ops()) == GELATO_OPS);
+
+        // check store was updated
+        assert(
+            marginAccountFactoryStorage.deployedMarginAccounts(address(this)) ==
+                address(account)
+        );
     }
+
+    /// @dev utility function doing same as above
+    /// @return account - MarginBase account
+    function createAccount() private returns (MarginBase account) {
+        // call factory to create account
+        account = MarginBase(payable(marginAccountFactory.newAccount()));
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                       ACCOUNT DEPOSITS/WITHDRAWS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice deposit sUSD into account
+    function testDeposit() external {}
+
+    /// @notice withdraw sUSD from account
+    function testWithdraw() external {}
 
     /*//////////////////////////////////////////////////////////////
                              BASIC TRADING
     //////////////////////////////////////////////////////////////*/
+    /// @dev basic trading excludes advanced/conditional 
+    /// orders such as limit/stop-loss
+
+    ///
+    /// OPENING POSITIONS
+    ///
+
+    /// @notice open a single long position
+    function testOpenLongPosition() external {}
+
+    /// @notice open multiple long positions
+    function testOpenLongPositions() external {}
+
+    /// @notice open a single short position
+    function testOpenShortPosition() external {}
+
+    /// @notice open multiple short positions
+    function testOpenShortPositions() external {}
+
+    /// @notice open multiple long and short positions
+    function testOpenPositions() external {}
+
+    ///
+    /// CLOSING POSITIONS
+    ///
+
+    /// @notice close a single long position
+    function testCloseLongPosition() external {}
+
+    /// @notice close multiple long positions
+    function testCloseLongPositions() external {}
+
+    /// @notice close a single short position
+    function testCloseShortPosition() external {}
+
+    /// @notice close multiple short positions
+    function testCloseShortPositions() external {}
+
+    /// @notice open multiple long and short positions
+    function testClosePositions() external {}
+
+    ///
+    /// MODIFYING POSITION MARGIN
+    ///
+
+    /// @notice open a single long position and then add margin
+    /// @param x: fuzzed value respresenting amount of margin to add
+    function testAddMarginToLong(uint256 x) external {}
+
+    /// @notice open a single short position and then add margin
+    /// @param x: fuzzed value respresenting amount of margin to add
+    function testAddMarginToShort(uint256 x) external {}
+
+    /// @notice open a single long position and then withdraw margin
+    /// @param x: fuzzed value respresenting amount of margin to withdraw
+    function testWithdrawMarginFromLong(uint256 x) external {}
+
+    /// @notice open a single short position and then withdraw margin
+    /// @param x: fuzzed value respresenting amount of margin to withdraw
+    function testWithdrawMarginFromShort(uint256 x) external {}
+
+    /// @notice open a single long position and attempt to withdraw all margin
+    function testRemoveAllMarginFromLong() external {}
+
+    /// @notice open a single short position and attempt to withdraw all margin
+    function testRemoveAllMarginFromShort() external {}
+
+    /// @notice open long and short positions in different markets
+    /// and in a single trade:
+    /// (1) + margin to some positions
+    /// (2) - margin from some positions
+    function testModifyMultiplePositionsMargin() external {}
+
+    ///
+    /// MODIFYING POSITION SIZE
+    ///
+
+    /// @notice open a single long position and then increase size
+    /// @param x: fuzzed value respresenting size increase
+    function testIncSizeOfLong(uint256 x) external {}
+
+    /// @notice open a single short position and then increase size
+    /// @param x: fuzzed value respresenting size increase
+    function testIncSizeOfShort(uint256 x) external {}
+
+    /// @notice open a single long position and then decrease size
+    /// @param x: fuzzed value respresenting size decrease
+    function testDecSizeOfLong(uint256 x) external {}
+
+    /// @notice open a single short position and then decrease size
+    /// @param x: fuzzed value respresenting size decrease
+    function testDecSizeOfShort(uint256 x) external {}
+
+    /// @notice open a single long position and attempt to decrease
+    /// size to zero
+    function testDecSizeToZeroOfLong() external {}
+
+    /// @notice open a single short position and attempt to decrease
+    /// size to zero
+    function testDecSizeToZeroOfShort() external {}
+
+    /// @notice open long and short positions in different markets
+    /// and in a single trade:
+    /// (1) + size of some positions
+    /// (2) - size of some positions
+    function testModifyMultiplePositionsMargin() external {}
 }
