35dd10857f5ada633df2ddb1432f5a967e245ad1
JaredBorders <jlbxsxs@gmail.com>
2023-01-30 15:49:12 -0500

    🪪 Update license

--- src/utils/OpsReady.sol
+++ src/utils/OpsReady.sol
@@ -1,9 +1,10 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.17;
 
 import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IOps} from "../interfaces/IOps.sol";
 
+/// @author JaredBorders (jaredborders@proton.me), JChiaramonte7 (jeremy@bytecode.llc)
 abstract contract OpsReady {
     address public ops;
     address public constant ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
