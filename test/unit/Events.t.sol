5d923510e6fa9d3071c4e562a592206a53026969
JaredBorders <jlbxsxs@gmail.com>
2023-03-09 11:13:41 -0500

    ✅ Test Events.sol

--- test/unit/Events.t.sol
+++ test/unit/Events.t.sol
@@ -3,15 +3,13 @@ pragma solidity 0.8.18;
 
 import "forge-std/Test.sol";
 import {Account} from "../../src/Account.sol";
-import {IAccount} from "../../src/interfaces/IAccount.sol";
 import {ConsolidatedEvents} from "../utils/ConsolidatedEvents.sol";
 import {Events} from "../../src/Events.sol";
 import {Factory} from "../../src/Factory.sol";
-import {IFactory} from "../../src/interfaces/IFactory.sol";
-import {MockAccount1, MockAccount2} from "../utils/MockAccounts.sol";
+import {IAccount} from "../../src/interfaces/IAccount.sol";
+import {IEvents} from "../../src/interfaces/IEvents.sol";
 import {Settings} from "../../src/Settings.sol";
 import {Setup} from "../../script/Deploy.s.sol";
-import {UpgradedAccount} from "../utils/UpgradedAccount.sol";
 import "../utils/Constants.sol";
 
 contract EventsTest is Test, ConsolidatedEvents {
@@ -23,7 +21,6 @@ contract EventsTest is Test, ConsolidatedEvents {
     Events private events;
     Factory private factory;
     Account private implementation;
-
     address private account;
 
     /*//////////////////////////////////////////////////////////////
@@ -45,8 +42,6 @@ contract EventsTest is Test, ConsolidatedEvents {
         settings = Settings(factory.settings());
         events = Events(factory.events());
         implementation = Account(payable(factory.implementation()));
-
-        // create account
         account = factory.newAccount();
     }
 
@@ -54,10 +49,27 @@ contract EventsTest is Test, ConsolidatedEvents {
                                  TESTS
     //////////////////////////////////////////////////////////////*/
 
+    /*//////////////////////////////////////////////////////////////
+                              CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+    function test_Constructor_FactorySett() public {
+        assertEq(events.factory(), address(factory));
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+
     function test_EmitDeposit_Event() public {
         vm.expectEmit(true, true, true, true);
         emit Deposit(USER, ACCOUNT, AMOUNT);
         vm.prank(account);
+        events.emitDeposit({user: USER, account: ACCOUNT, amount: AMOUNT});
+    }
+
+    function test_EmitDeposit_OnlyAccounts() public {
+        vm.expectRevert(abi.encodeWithSelector(IEvents.OnlyAccounts.selector));
         events.emitDeposit({user: USER, account: ACCOUNT, amount: AMOUNT});
     }
 
@@ -68,10 +80,20 @@ contract EventsTest is Test, ConsolidatedEvents {
         events.emitWithdraw({user: USER, account: ACCOUNT, amount: AMOUNT});
     }
 
+    function test_EmitWithdraw_OnlyAccounts() public {
+        vm.expectRevert(abi.encodeWithSelector(IEvents.OnlyAccounts.selector));
+        events.emitWithdraw({user: USER, account: ACCOUNT, amount: AMOUNT});
+    }
+
     function test_EmitEthWithdraw_Event() public {
         vm.expectEmit(true, true, true, true);
         emit EthWithdraw(USER, ACCOUNT, AMOUNT);
         vm.prank(account);
+        events.emitEthWithdraw({user: USER, account: ACCOUNT, amount: AMOUNT});
+    }
+
+    function test_EmitEthWithdraw_OnlyAccounts() public {
+        vm.expectRevert(abi.encodeWithSelector(IEvents.OnlyAccounts.selector));
         events.emitEthWithdraw({user: USER, account: ACCOUNT, amount: AMOUNT});
     }
 
@@ -103,6 +125,21 @@ contract EventsTest is Test, ConsolidatedEvents {
         });
     }
 
+    function test_EmitConditionalOrderPlaced_OnlyAccounts() public {
+        vm.expectRevert(abi.encodeWithSelector(IEvents.OnlyAccounts.selector));
+        events.emitConditionalOrderPlaced({
+            account: ACCOUNT,
+            conditionalOrderId: 0,
+            marketKey: sETHPERP,
+            marginDelta: MARGIN_DELTA,
+            sizeDelta: SIZE_DELTA,
+            targetPrice: TARGET_PRICE,
+            conditionalOrderType: IAccount.ConditionalOrderTypes.LIMIT,
+            priceImpactDelta: PRICE_IMPACT_DELTA,
+            reduceOnly: true
+        });
+    }
+
     function test_EmitConditionalOrderCancelled_Event() public {
         uint256 id = 0;
         vm.expectEmit(true, true, true, true);
@@ -123,6 +160,17 @@ contract EventsTest is Test, ConsolidatedEvents {
         });
     }
 
+    function test_EmitConditionalOrderCancelled_OnlyAccounts() public {
+        vm.expectRevert(abi.encodeWithSelector(IEvents.OnlyAccounts.selector));
+        events.emitConditionalOrderCancelled({
+            account: ACCOUNT,
+            conditionalOrderId: 0,
+            reason: IAccount
+                .ConditionalOrderCancelledReason
+                .CONDITIONAL_ORDER_CANCELLED_BY_USER
+        });
+    }
+
     function test_EmitConditionalOrderFilled_Event() public {
         vm.expectEmit(true, true, true, true);
         emit ConditionalOrderFilled(ACCOUNT, 0, FILL_PRICE, GELATO_FEE);
@@ -135,10 +183,25 @@ contract EventsTest is Test, ConsolidatedEvents {
         });
     }
 
+    function test_EmitConditionalOrderFilled_OnlyAccounts() public {
+        vm.expectRevert(abi.encodeWithSelector(IEvents.OnlyAccounts.selector));
+        events.emitConditionalOrderFilled({
+            account: ACCOUNT,
+            conditionalOrderId: 0,
+            fillPrice: FILL_PRICE,
+            keeperFee: GELATO_FEE
+        });
+    }
+
     function test_EmitFeeImposed_Event() public {
         vm.expectEmit(true, true, true, true);
         emit FeeImposed(ACCOUNT, AMOUNT);
         vm.prank(account);
+        events.emitFeeImposed({account: ACCOUNT, amount: AMOUNT});
+    }
+
+    function test_EmitFeeImposed_OnlyAccounts() public {
+        vm.expectRevert(abi.encodeWithSelector(IEvents.OnlyAccounts.selector));
         events.emitFeeImposed({account: ACCOUNT, amount: AMOUNT});
     }
 }
