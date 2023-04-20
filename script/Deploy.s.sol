5390fd09784a4bad8099feee2e2c96f1782de5b6
JaredBorders <jlbxsxs@gmail.com>
2023-04-20 14:49:18 -0400

    🚀 Update deployment

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,13 +2,12 @@
 pragma solidity 0.8.18;
 
 import "forge-std/Script.sol";
+import "./utils/deploy-parameters.sol";
 import {Account} from "src/Account.sol";
 import {Events} from "src/Events.sol";
 import {Factory} from "src/Factory.sol";
-
-interface IAddressResolver {
-    function getAddress(bytes32 name) external view returns (address);
-}
+import {IAddressResolver} from "./utils/interfaces/IAddressResolver.sol";
+import {Settings} from "src/Settings.sol";
 
 /// @title Script to deploy Kwenta's Smart Margin Account Factory
 /// @author JaredBorders (jaredborders@pm.me)
@@ -19,16 +18,33 @@ contract Setup {
         address _addressResolver,
         address _gelato,
         address _ops
-    ) public returns (Factory factory, Events events, Account implementation) {
+    )
+        public
+        returns (
+            Factory factory,
+            Events events,
+            Settings settings,
+            Account implementation
+        )
+    {
         // define *initial* factory owner
         address temporaryOwner =
             _deployer == address(0) ? address(this) : _deployer;
 
         // deploy the factory
-        factory = deploySmartMarginFactory({_owner: temporaryOwner});
+        factory = new Factory({
+            _owner: temporaryOwner
+        });
 
         // deploy the events contract and set the factory
-        events = deployEvents({_factory: address(factory)});
+        events = new Events({
+            _factory: address(factory)
+        });
+
+        // deploy the settings contract
+        settings = new Settings({
+            _owner: _owner
+        });
 
         // resolve necessary addresses via the Synthetix Address Resolver
         IAddressResolver addressResolver = IAddressResolver(_addressResolver);
@@ -39,15 +55,15 @@ contract Setup {
         address systemStatus =
             addressResolver.getAddress({name: bytes32("SystemStatus")});
 
-        // deploy the account implementation
-        implementation = deployAccountImplementation({
+        implementation = new Account({
             _factory: address(factory),
             _events: address(events),
             _marginAsset: marginAsset,
             _futuresMarketManager: futuresMarketManager,
             _systemStatus: systemStatus,
             _gelato: _gelato,
-            _ops: _ops
+            _ops: _ops,
+            _settings: address(settings)
         });
 
         // update the factory with the new account implementation
@@ -58,64 +74,22 @@ contract Setup {
         // transfer ownership of the factory to the owner
         factory.transferOwnership({newOwner: _owner});
     }
-
-    function deploySmartMarginFactory(address _owner)
-        internal
-        returns (Factory factory)
-    {
-        factory = new Factory({
-            _owner: _owner
-        });
-    }
-
-    function deployEvents(address _factory) internal returns (Events events) {
-        events = new Events({
-            _factory: _factory
-        });
-    }
-
-    function deployAccountImplementation(
-        address _factory,
-        address _events,
-        address _marginAsset,
-        address _futuresMarketManager,
-        address _systemStatus,
-        address _gelato,
-        address _ops
-    ) internal returns (Account implementation) {
-        implementation = new Account({
-            _factory: _factory,
-            _events: _events,
-            _marginAsset: _marginAsset,
-            _futuresMarketManager: _futuresMarketManager,
-            _systemStatus: _systemStatus,
-            _gelato: _gelato,
-            _ops: _ops
-        });
-    }
 }
 
 /// @dev steps to deploy and verify on Optimism:
 /// (1) load the variables in the .env file via `source .env`
 /// (2) run `forge script script/Deploy.s.sol:DeployOptimism --rpc-url $ARCHIVE_NODE_URL_L2 --broadcast --verify -vvvv`
 contract DeployOptimism is Script, Setup {
-    address private constant KWENTA_ADMIN_DAO_MULTI_SIG =
-        0xF510a2Ff7e9DD7e18629137adA4eb56B9c13E885;
-    address private constant SYNTHETIX_ADDRESS_RESOLVER =
-        0x1Cb059b7e74fD21665968C908806143E744D5F30;
-    address private constant GELATO = 0x01051113D81D7d6DA508462F2ad6d7fD96cF42Ef;
-    address private constant OPS = 0x340759c8346A1E6Ed92035FB8B6ec57cE1D82c2c;
-
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
         Setup.deploySystem({
-            _deployer: 0x39CFcA7b389529ac861CbB05aDD802e5B06E5101,
-            _owner: KWENTA_ADMIN_DAO_MULTI_SIG,
-            _addressResolver: SYNTHETIX_ADDRESS_RESOLVER,
-            _gelato: GELATO,
-            _ops: OPS
+            _deployer: OPTIMISM_DEPLOYER,
+            _owner: OPTIMISM_KWENTA_ADMIN_DAO_MULTI_SIG,
+            _addressResolver: OPTIMISM_SYNTHETIX_ADDRESS_RESOLVER,
+            _gelato: OPTIMISM_GELATO,
+            _ops: OPTIMISM_OPS
         });
 
         vm.stopBroadcast();
@@ -125,25 +99,17 @@ contract DeployOptimism is Script, Setup {
 /// @dev steps to deploy and verify on Optimism Goerli:
 /// (1) load the variables in the .env file via `source .env`
 /// (2) run `forge script script/Deploy.s.sol:DeployOptimismGoerli --rpc-url $ARCHIVE_NODE_URL_GOERLI_L2 --broadcast --verify -vvvv`
-/// @dev here the KWENTA_ADMIN_DAO_MULTI_SIG is the deployer address
 contract DeployOptimismGoerli is Script, Setup {
-    address private constant KWENTA_ADMIN_DAO_MULTI_SIG =
-        0xc625F59d51ecDff57FEFE535C80d318CA42A0Ec4;
-    address private constant SYNTHETIX_ADDRESS_RESOLVER =
-        0x9Fc84992dF5496797784374B810E04238728743d;
-    address private constant GELATO = 0xF82D64357D9120a760e1E4C75f646C0618eFc2F3;
-    address private constant OPS = 0x255F82563b5973264e89526345EcEa766DB3baB2;
-
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
         Setup.deploySystem({
-            _deployer: 0xc625F59d51ecDff57FEFE535C80d318CA42A0Ec4,
-            _owner: KWENTA_ADMIN_DAO_MULTI_SIG,
-            _addressResolver: SYNTHETIX_ADDRESS_RESOLVER,
-            _gelato: GELATO,
-            _ops: OPS
+            _deployer: OPTIMISM_GOERLI_DEPLOYER,
+            _owner: OPTIMISM_GOERLI_KWENTA_ADMIN_DAO_MULTI_SIG,
+            _addressResolver: OPTIMISM_GOERLI_SYNTHETIX_ADDRESS_RESOLVER,
+            _gelato: OPTIMISM_GOERLI_GELATO,
+            _ops: OPTIMISM_GOERLI_OPS
         });
 
         vm.stopBroadcast();
