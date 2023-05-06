6c5e2729221191caae40c330d1fba66968398baa
JaredBorders <jlbxsxs@gmail.com>
2023-05-07 02:02:24 +0300

    📚 Update docs

--- src/Settings.sol
+++ src/Settings.sol
@@ -5,10 +5,26 @@ import {ISettings} from "./interfaces/ISettings.sol";
 import {Owned} from "@solmate/auth/Owned.sol";
 
 contract Settings is ISettings, Owned {
+    /*//////////////////////////////////////////////////////////////
+                                 STATE
+    //////////////////////////////////////////////////////////////*/
+    
+    /// @inheritdoc ISettings
     bool public accountExecutionEnabled = true;
 
+    /*//////////////////////////////////////////////////////////////
+                              CONSTRUCTOR
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice constructs the Settings contract
+    /// @param _owner: address of the owner of the contract
     constructor(address _owner) Owned(_owner) {}
 
+    /*//////////////////////////////////////////////////////////////
+                                SETTERS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @inheritdoc ISettings
     function setAccountExecutionEnabled(bool _enabled) external onlyOwner {
         accountExecutionEnabled = _enabled;
         emit AccountExecutionEnabledSet(_enabled);
--- src/interfaces/ISettings.sol
+++ src/interfaces/ISettings.sol
@@ -2,8 +2,27 @@
 pragma solidity 0.8.18;
 
 interface ISettings {
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice emitted when account execution is enabled or disabled
+    /// @param enabled: true if account execution is enabled, false if disabled
     event AccountExecutionEnabledSet(bool enabled);
 
+    /*//////////////////////////////////////////////////////////////
+                                 VIEWS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice checks if account execution is enabled or disabled
+    /// @return enabled: true if account execution is enabled, false if disabled
     function accountExecutionEnabled() external view returns (bool);
+
+    /*//////////////////////////////////////////////////////////////
+                                SETTERS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice enables or disables account execution
+    /// @param _enabled: true if account execution is enabled, false if disabled
     function setAccountExecutionEnabled(bool _enabled) external;
 }
