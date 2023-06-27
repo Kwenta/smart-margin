922d2a39e17fb5ecb1e25cd36d7b081db530fff6
JaredBorders <jlbxsxs@gmail.com>
2023-06-26 21:03:57 -0400

    🧹 Clean up interface

--- src/interfaces/uniswap/IEIP712.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.8.18;
-
-interface IEIP712 {
-    function DOMAIN_SEPARATOR() external view returns (bytes32);
-}
--- src/interfaces/uniswap/IPermit2.sol
+++ src/interfaces/uniswap/IPermit2.sol
@@ -1,11 +1,76 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.18;
 
-import {ISignatureTransfer} from "./ISignatureTransfer.sol";
-import {IAllowanceTransfer} from "./IAllowanceTransfer.sol";
+interface IPermit2 {
+    /// @notice The permit data for a token
+    struct PermitDetails {
+        // ERC20 token address
+        address token;
+        // the maximum amount allowed to spend
+        uint160 amount;
+        // timestamp at which a spender's token allowances become invalid
+        uint48 expiration;
+        // an incrementing value indexed per owner,token,and spender for each signature
+        uint48 nonce;
+    }
 
-/// @notice Permit2 handles signature-based transfers in SignatureTransfer and allowance-based transfers in AllowanceTransfer.
-/// @dev Users must approve Permit2 before calling any of the transfer functions.
-interface IPermit2 is ISignatureTransfer, IAllowanceTransfer {
-// IPermit2 unifies the two interfaces so users have maximal flexibility with their approval.
+    /// @notice The permit message signed for a single token allownce
+    struct PermitSingle {
+        // the permit data for a single token alownce
+        PermitDetails details;
+        // address permissioned on the allowed tokens
+        address spender;
+        // deadline on the permit signature
+        uint256 sigDeadline;
+    }
+
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+
+    /// @dev Should return whether the signature provided is valid for the provided data
+    /// @param hash      Hash of the data to be signed
+    /// @param signature Signature byte array associated with _data
+    /// @return magicValue The bytes4 magic value 0x1626ba7e
+    function isValidSignature(bytes32 hash, bytes memory signature)
+        external
+        view
+        returns (bytes4 magicValue);
+
+    /// @notice Approves the spender to use up to amount of the specified token up until the expiration
+    /// @param token The token to approve
+    /// @param spender The spender address to approve
+    /// @param amount The approved amount of the token
+    /// @param expiration The timestamp at which the approval is no longer valid
+    /// @dev The packed allowance also holds a nonce, which will stay unchanged in approve
+    /// @dev Setting amount to type(uint160).max sets an unlimited approval
+    function approve(
+        address token,
+        address spender,
+        uint160 amount,
+        uint48 expiration
+    ) external;
+
+    /// @notice Permit a spender to a given amount of the owners token via the owner's EIP-712 signature
+    /// @dev May fail if the owner's nonce was invalidated in-flight by invalidateNonce
+    /// @param owner The owner of the tokens being approved
+    /// @param permitSingle Data signed over by the owner specifying the terms of approval
+    /// @param signature The owner's signature over the permit data
+    function permit(
+        address owner,
+        PermitSingle memory permitSingle,
+        bytes calldata signature
+    ) external;
+
+    /// @notice Transfer approved tokens from one address to another
+    /// @param from The address to transfer from
+    /// @param to The address of the recipient
+    /// @param amount The amount of the token to transfer
+    /// @param token The token address to transfer
+    /// @dev Requires the from address to have approved at least the desired amount
+    /// of tokens to msg.sender.
+    function transferFrom(
+        address from,
+        address to,
+        uint160 amount,
+        address token
+    ) external;
 }
