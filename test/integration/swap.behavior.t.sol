a51ba83db6a62927d400e9798335dfde06346083
JaredBorders <jlbxsxs@gmail.com>
2023-07-03 17:47:24 -0500

    ✅ Test invalid swaps

--- test/integration/swap.behavior.t.sol
+++ test/integration/swap.behavior.t.sol
@@ -23,7 +23,8 @@ import {
     PROXY_SUSD,
     UNISWAP_PERMIT2,
     UNISWAP_UNIVERSAL_ROUTER,
-    USDC
+    USDC,
+    USER
 } from "../utils/Constants.sol";
 
 contract SwapBehaviorTest is Test, ConsolidatedEvents {
@@ -288,43 +289,227 @@ contract SwapBehaviorTest is Test, ConsolidatedEvents {
     //////////////////////////////////////////////////////////////*/
 
     function test_UniswapV3Swap_Only_Whitelisted_TokenIn() public {
-        /// @custom:todo add test; tokenIn must be whitelisted if tokenOut is sUSD
+        dai.approve(UNISWAP_PERMIT2, type(uint256).max);
+
+        PERMIT2.approve(
+            address(dai), address(account), type(uint160).max, type(uint48).max
+        );
+
+        IAccount.Command[] memory commands = new IAccount.Command[](1);
+        commands[0] = IAccount.Command.UNISWAP_V3_SWAP;
+
+        bytes[] memory inputs = new bytes[](1);
+        uint256 amountIn = AMOUNT / 2;
+        uint256 amountOutMin = 1;
+        bytes memory path = bytes.concat(
+            bytes20(address(dai)), LOW_FEE_TIER, bytes20(address(sUSD))
+        );
+        inputs[0] = abi.encode(amountIn, amountOutMin, path);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAccount.TokenSwapNotAllowed.selector,
+                address(dai),
+                address(sUSD)
+            )
+        );
+        account.execute(commands, inputs);
     }
 
     function test_UniswapV3Swap_Only_Whitelisted_TokenOut() public {
-        /// @custom:todo add test; tokenOut must be whitelisted if tokenIn is sUSD
+        fundAccount(AMOUNT);
+
+        IAccount.Command[] memory commands = new IAccount.Command[](1);
+        commands[0] = IAccount.Command.UNISWAP_V3_SWAP;
+
+        bytes[] memory inputs = new bytes[](1);
+        uint256 amountIn = AMOUNT / 2;
+        uint256 amountOutMin = 1;
+        bytes memory path = bytes.concat(
+            bytes20(address(sUSD)), LOW_FEE_TIER, bytes20(address(dai))
+        );
+        inputs[0] = abi.encode(amountIn, amountOutMin, path);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAccount.TokenSwapNotAllowed.selector,
+                address(sUSD),
+                address(dai)
+            )
+        );
+        account.execute(commands, inputs);
     }
 
     function test_UniswapV3Swap_Either_SUSD_In_Out() public {
-        /// @custom:todo add test; sUSD must be either tokenIn or tokenOut
+        /// @notice sUSD must be either tokenIn or tokenOut
+
+        settings.setTokenWhitelistStatus(address(dai), true);
+
+        dai.approve(UNISWAP_PERMIT2, type(uint256).max);
+
+        PERMIT2.approve(
+            address(dai), address(account), type(uint160).max, type(uint48).max
+        );
+
+        IAccount.Command[] memory commands = new IAccount.Command[](1);
+        commands[0] = IAccount.Command.UNISWAP_V3_SWAP;
+
+        bytes[] memory inputs = new bytes[](1);
+        uint256 amountIn = AMOUNT / 2;
+        uint256 amountOutMin = 1;
+        bytes memory path = bytes.concat(
+            bytes20(address(dai)), LOW_FEE_TIER, bytes20(address(dai))
+        );
+        inputs[0] = abi.encode(amountIn, amountOutMin, path);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAccount.TokenSwapNotAllowed.selector,
+                address(dai),
+                address(dai)
+            )
+        );
+        account.execute(commands, inputs);
     }
 
     function test_UniswapV3Swap_Invalid_AmountIn() public {
-        /// @custom:todo add test; amountIn must be > 0
+        /// @notice amountIn must be > 0
+
+        settings.setTokenWhitelistStatus(address(dai), true);
+
+        dai.approve(UNISWAP_PERMIT2, type(uint256).max);
+
+        PERMIT2.approve(
+            address(dai), address(account), type(uint160).max, type(uint48).max
+        );
+
+        IAccount.Command[] memory commands = new IAccount.Command[](1);
+        commands[0] = IAccount.Command.UNISWAP_V3_SWAP;
+
+        bytes[] memory inputs = new bytes[](1);
+        uint256 amountIn = 0;
+        uint256 amountOutMin = 1;
+        bytes memory path = bytes.concat(
+            bytes20(address(dai)), LOW_FEE_TIER, bytes20(address(sUSD))
+        );
+        inputs[0] = abi.encode(amountIn, amountOutMin, path);
+
+        /// 'AS' revert message from UniswapV3Pool.sol when amountIn is 0
+        /// https://docs.uniswap.org/contracts/v3/reference/error-codes
+        vm.expectRevert(abi.encodePacked("AS"));
+        account.execute(commands, inputs);
     }
 
     function test_UniswapV3Swap_Invalid_AmountOutMin() public {
-        /// @custom:todo add test; if amountOutMin is greater than amountOut, swap should fail
+        /// @notice add test; if amountOutMin is greater than amountOut, swap should fail
+
+        settings.setTokenWhitelistStatus(address(dai), true);
+
+        dai.approve(UNISWAP_PERMIT2, type(uint256).max);
+
+        PERMIT2.approve(
+            address(dai), address(account), type(uint160).max, type(uint48).max
+        );
+
+        IAccount.Command[] memory commands = new IAccount.Command[](1);
+        commands[0] = IAccount.Command.UNISWAP_V3_SWAP;
+
+        bytes[] memory inputs = new bytes[](1);
+        uint256 amountIn = AMOUNT / 2;
+        uint256 amountOutMin = type(uint256).max;
+        bytes memory path = bytes.concat(
+            bytes20(address(dai)), LOW_FEE_TIER, bytes20(address(sUSD))
+        );
+        inputs[0] = abi.encode(amountIn, amountOutMin, path);
+
+        /// Custom Error type `V3TooLittleReceived` with signature `V3TooLittleReceived()`
+        /// https://github.com/Uniswap/universal-router/blob/471d99ba276c383d2353d065f7a682e710ca7bdc/contracts/modules/uniswap/v3/V3SwapRouter.sol#L114
+        vm.expectRevert(abi.encodeWithSignature("V3TooLittleReceived()"));
+        account.execute(commands, inputs);
     }
 
     function test_UniswapV3Swap_Invalid_Path() public {
         bytes memory invalidPath = bytes.concat(
-            bytes20(address(dai)),
+            bytes20(address(sUSD)),
             LOW_FEE_TIER,
             bytes20(address(0xBEEF)), // invalid token; will cause swap to fail
             LOW_FEE_TIER,
-            bytes20(address(sUSD))
+            bytes20(address(dai))
         );
 
-        /// @custom:todo add test; if path is invalid, swap should fail
+        settings.setTokenWhitelistStatus(address(dai), true);
+
+        fundAccount(AMOUNT);
+
+        IAccount.Command[] memory commands = new IAccount.Command[](1);
+        commands[0] = IAccount.Command.UNISWAP_V3_SWAP;
+
+        bytes[] memory inputs = new bytes[](1);
+        uint256 amountIn = AMOUNT / 2;
+        uint256 amountOutMin = 1;
+        inputs[0] = abi.encode(amountIn, amountOutMin, invalidPath);
+
+        // Call reverts without data
+        vm.expectRevert(abi.encodePacked(""));
+        account.execute(commands, inputs);
     }
 
     function test_UniswapV3Swap_Insufficient_Margin() public {
-        /// @custom:todo add test; if account has insufficient margin when swapping out of sUSD, swap should fail
+        /// @notice if account has insufficient margin when swapping out of sUSD, swap should fail
+
+        settings.setTokenWhitelistStatus(address(dai), true);
+
+        IAccount.Command[] memory commands = new IAccount.Command[](1);
+        commands[0] = IAccount.Command.UNISWAP_V3_SWAP;
+
+        bytes[] memory inputs = new bytes[](1);
+        uint256 amountIn = AMOUNT / 2;
+        uint256 amountOutMin = 1;
+        bytes memory path = bytes.concat(
+            bytes20(address(sUSD)), LOW_FEE_TIER, bytes20(address(dai))
+        );
+        inputs[0] = abi.encode(amountIn, amountOutMin, path);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IAccount.InsufficientFreeMargin.selector, 
+                0, 
+                AMOUNT / 2
+            )
+        );
+        account.execute(commands, inputs);
     }
 
     function test_UniswapV3Swap_Insufficient_TokenIn_Balance() public {
-        /// @custom:todo add test; if EOA has insufficient tokenIn balance, swap should fail
+        /// @notice if EOA has insufficient tokenIn balance, swap should fail
+
+        settings.setTokenWhitelistStatus(address(dai), true);
+
+        account.transferOwnership(USER);
+
+        vm.startPrank(USER); // USER does not have DAI
+
+        dai.approve(UNISWAP_PERMIT2, type(uint256).max);
+
+        PERMIT2.approve(
+            address(dai), address(account), type(uint160).max, type(uint48).max
+        );
+
+        IAccount.Command[] memory commands = new IAccount.Command[](1);
+        commands[0] = IAccount.Command.UNISWAP_V3_SWAP;
+
+        bytes[] memory inputs = new bytes[](1);
+        uint256 amountIn = AMOUNT / 2;
+        uint256 amountOutMin = 1;
+        bytes memory path = bytes.concat(
+            bytes20(address(dai)), LOW_FEE_TIER, bytes20(address(sUSD))
+        );
+        inputs[0] = abi.encode(amountIn, amountOutMin, path);
+
+        vm.expectRevert("TRANSFER_FROM_FAILED");
+        account.execute(commands, inputs);
+
+         vm.stopPrank();
     }
 
     /*//////////////////////////////////////////////////////////////
