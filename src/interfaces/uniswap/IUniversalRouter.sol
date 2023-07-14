94c03ea9acffea28e99aecf7515259ac3631a690
JaredBorders <jlbxsxs@gmail.com>
2023-07-14 14:48:05 -0400

    ✨ Prettify

--- src/interfaces/uniswap/IUniversalRouter.sol
+++ src/interfaces/uniswap/IUniversalRouter.sol
@@ -15,8 +15,7 @@ interface IUniversalRouter {
     /// @notice Executes encoded commands along with provided inputs
     /// @param commands A set of concatenated commands, each 1 byte in length
     /// @param inputs An array of byte strings containing abi encoded inputs for each command
-    function execute(
-        bytes calldata commands,
-        bytes[] calldata inputs
-    ) external payable;
+    function execute(bytes calldata commands, bytes[] calldata inputs)
+        external
+        payable;
 }
