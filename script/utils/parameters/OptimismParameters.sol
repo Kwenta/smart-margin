4d43b53b2f16e7caef90205716fbba7e658a03bb
JaredBorders <jlbxsxs@gmail.com>
2023-08-09 11:46:34 -0400

    🚀 Deploy Mykonos Upgrade

--- script/utils/parameters/OptimismParameters.sol
+++ script/utils/parameters/OptimismParameters.sol
@@ -16,18 +16,18 @@ address constant OPTIMISM_GELATO = 0x01051113D81D7d6DA508462F2ad6d7fD96cF42Ef;
 
 address constant OPTIMISM_OPS = 0x340759c8346A1E6Ed92035FB8B6ec57cE1D82c2c;
 
-// v2.0.2
+// v2.1.0
 address constant OPTIMISM_IMPLEMENTATION =
-    0x3BC8D34314E77c2E36948Fd8F4B353f1baDc3F6C;
+    0x83E13069aA457778ca349E0128927B417A2c2B3f;
 
-// released with v2.0.2 implementation
-address constant OPTIMISM_EVENTS = 0x11193470df30B37Af9fc5Ec696c240D878bdfb42;
+// released with v2.1.0 implementation
+address constant OPTIMISM_EVENTS = 0xB753d2EE5dcA1fF39A83CA3Ec500656c31Be940b;
 
-// updated with v2.0.2 implementation
+// updated with v2.1.0 implementation
 address constant OPTIMISM_FACTORY = 0x8234F990b149Ae59416dc260305E565e5DAfEb54;
 
-// released with v2.0.1 implementation
-address constant OPTIMISM_SETTINGS = 0xD02813baF080d06FC6F706cF93F5DaA96D6edB17;
+// released with v2.1.0 implementation
+address constant OPTIMISM_SETTINGS = 0x865dA103d126b3Be3599D84caB57109A861F5631;
 
 // key(s) used by Synthetix address resolver
 bytes32 constant PROXY_SUSD = "ProxysUSD";
