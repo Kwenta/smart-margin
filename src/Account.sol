ed375ba27a600f61de3229e05450a062f22ddda3
JaredBorders <jlbxsxs@gmail.com>
2023-09-26 15:26:39 -0400

    ⚙️ Update version

--- src/Account.sol
+++ src/Account.sol
@@ -37,7 +37,7 @@ contract Account is IAccount, Auth, OpsReady {
     //////////////////////////////////////////////////////////////*/
 
     /// @inheritdoc IAccount
-    bytes32 public constant VERSION = "2.1.0";
+    bytes32 public constant VERSION = "2.1.1";
 
     /// @notice tracking code used when modifying positions
     bytes32 internal constant TRACKING_CODE = "KWENTA";
--- test/unit/Account.t.sol
+++ test/unit/Account.t.sol
@@ -118,7 +118,7 @@ contract AccountTest is Test, ConsolidatedEvents {
     //////////////////////////////////////////////////////////////*/
 
     function test_GetVerison() public view {
-        assert(account.VERSION() == "2.1.0");
+        assert(account.VERSION() == "2.1.1");
     }
 
     function test_GetTrackingCode() public view {
