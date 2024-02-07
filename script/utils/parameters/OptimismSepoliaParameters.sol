fe66fa53d45193d0432847f6b671a2945b3089e8
JaredBorders <jaredborders@pm.me>
2024-02-07 12:03:29 -0500

    🚀 add op sepolia deployment

--- script/utils/parameters/OptimismSepoliaParameters.sol
+++ script/utils/parameters/OptimismSepoliaParameters.sol
@@ -19,19 +19,25 @@ address constant OPTIMISM_SEPOLIA_GELATO = address(0);
 // not deployed yet
 address constant OPTIMISM_SEPOLIA_OPS = address(0);
 
-address constant OPTIMISM_SEPOLIA_IMPLEMENTATION = address(0);
+address constant OPTIMISM_SEPOLIA_IMPLEMENTATION =
+    0xe5bB889B1f0B6B4B7384Bd19cbb37adBDDa941a6;
 
-address constant OPTIMISM_SEPOLIA_EVENTS = address(0);
+address constant OPTIMISM_SEPOLIA_EVENTS =
+    0xd5fE5beAa04270B32f81Bf161768c44DF9880D11;
 
-address constant OPTIMISM_SEPOLIA_FACTORY = address(0);
+address constant OPTIMISM_SEPOLIA_FACTORY =
+    0xF877315CfC91E69e7f4c308ec312cf91D66a095F;
 
-address constant OPTIMISM_SEPOLIA_SETTINGS = address(0);
+address constant OPTIMISM_SEPOLIA_SETTINGS =
+    0x33B725a1B2dE9178121D423D2A1c062C5452f310;
 
 // uniswap v3:
 // UniversalRouterV1_2
+/// @custom:caution not official address
 address constant OPTIMISM_SEPOLIA_UNISWAP_UNIVERSAL_ROUTER =
     0xD5bBa708b39537d33F2812E5Ea032622456F1A95;
 
 // PERMIT2
+/// @custom:caution assumes same address as on mainnet
 address constant OPTIMISM_SEPOLIA_UNISWAP_PERMIT2 =
     0x000000000022D473030F116dDEE9F6B43aC78BA3;
