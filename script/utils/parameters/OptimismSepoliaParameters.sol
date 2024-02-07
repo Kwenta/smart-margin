54353a87fa154dd71c4becd05be7ff5eef1976ca
JaredBorders <jaredborders@pm.me>
2024-02-07 11:41:10 -0500

    🚀 add op sepolia deployment params

---
+++ script/utils/parameters/OptimismSepoliaParameters.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.18;
+
+/// @dev for Synthetix addresses see:
+/// https://github.com/Synthetixio/synthetix-docs/blob/master/content/addresses.md#sepolia-optimism-l2
+
+address constant OPTIMISM_SEPOLIA_DEPLOYER =
+    0x12d970154Ac171293323f20757130d5731850deB;
+
+address constant OPTIMISM_SEPOLIA_PDAO =
+    0x12d970154Ac171293323f20757130d5731850deB;
+
+address constant OPTIMISM_SEPOLIA_SYNTHETIX_ADDRESS_RESOLVER =
+    0x352436A7d39F7250bfe2D3E1EC679A6e87c2F715;
+
+// not deployed yet
+address constant OPTIMISM_SEPOLIA_GELATO = address(0);
+
+// not deployed yet
+address constant OPTIMISM_SEPOLIA_OPS = address(0);
+
+address constant OPTIMISM_SEPOLIA_IMPLEMENTATION = address(0);
+
+address constant OPTIMISM_SEPOLIA_EVENTS = address(0);
+
+address constant OPTIMISM_SEPOLIA_FACTORY = address(0);
+
+address constant OPTIMISM_SEPOLIA_SETTINGS = address(0);
+
+// uniswap v3:
+// UniversalRouterV1_2
+address constant OPTIMISM_SEPOLIA_UNISWAP_UNIVERSAL_ROUTER =
+    0xD5bBa708b39537d33F2812E5Ea032622456F1A95;
+
+// PERMIT2
+address constant OPTIMISM_SEPOLIA_UNISWAP_PERMIT2 =
+    0x000000000022D473030F116dDEE9F6B43aC78BA3;
