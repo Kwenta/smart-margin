782a387e51cec6039c81bcd075b4ab94542d3944
JaredBorders <jaredborders@pm.me>
2024-05-10 17:31:24 -0400

    🚀 add op sepolia upgrade script

--- script/upgrades/v2.1.4/Upgrade.s.sol
+++ script/upgrades/v2.1.4/Upgrade.s.sol
@@ -24,6 +24,16 @@ import {
     OPTIMISM_PDAO
 } from "script/utils/parameters/OptimismParameters.sol";
 
+import {
+    OPTIMISM_SEPOLIA_DEPLOYER,
+    OPTIMISM_SEPOLIA_SYNTHETIX_ADDRESS_RESOLVER,
+    OPTIMISM_SEPOLIA_GELATO,
+    OPTIMISM_SEPOLIA_OPS,
+    OPTIMISM_SEPOLIA_FACTORY,
+    OPTIMISM_SEPOLIA_UNISWAP_UNIVERSAL_ROUTER,
+    OPTIMISM_SEPOLIA_UNISWAP_PERMIT2
+} from "script/utils/parameters/OptimismSepoliaParameters.sol";
+
 /// @title Script to upgrade the Account implementation v2.1.3 -> v2.1.4
 /// @author JaredBorders (jaredborders@pm.me)
 
@@ -79,6 +89,64 @@ contract UpgradeAccountOptimism is Script {
             settings: settings,
             universalRouter: OPTIMISM_UNISWAP_UNIVERSAL_ROUTER,
             permit2: OPTIMISM_UNISWAP_PERMIT2
+        });
+
+        implementation = address(new Account(params));
+    }
+}
+
+/// @dev steps to deploy and verify on Optimism Sepolia:
+/// (1) load the variables in the .env file via `source .env`
+/// (2) run `forge script script/upgrades/v2.1.4/Upgrade.s.sol:UpgradeAccountOptimismSepolia --rpc-url $ARCHIVE_NODE_URL_SEPOLIA_L2 --broadcast --verify -vvvv`
+/// (3) Smart Margin Account Factory owner (i.e. Kwenta pDAO) will need to call `upgradeAccountImplementation` on the Factory with the address of the new Account implementation
+contract UpgradeAccountOptimismSepolia is Script {
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        upgrade();
+
+        vm.stopBroadcast();
+    }
+
+    function upgrade() public returns (address implementation) {
+        IAddressResolver addressResolver =
+            IAddressResolver(OPTIMISM_SEPOLIA_SYNTHETIX_ADDRESS_RESOLVER);
+
+        address marginAsset = addressResolver.getAddress({name: PROXY_SUSD});
+        address perpsV2ExchangeRate =
+            addressResolver.getAddress({name: PERPS_V2_EXCHANGE_RATE});
+        address futuresMarketManager =
+            addressResolver.getAddress({name: FUTURES_MARKET_MANAGER});
+        address systemStatus = addressResolver.getAddress({name: SYSTEM_STATUS});
+
+        address events =
+            address(new Events({_factory: OPTIMISM_SEPOLIA_FACTORY}));
+
+        address settings =
+            address(new Settings({_owner: OPTIMISM_SEPOLIA_DEPLOYER}));
+
+        // Order flow fee set to 0.5 BPS
+        Settings(settings).setOrderFlowFee(5);
+
+        // USDC token address whitelisted
+        Settings(settings).setTokenWhitelistStatus(
+            0x5fd84259d66Cd46123540766Be93DFE6D43130D7, true
+        );
+
+        IAccount.AccountConstructorParams memory params = IAccount
+            .AccountConstructorParams({
+            factory: OPTIMISM_SEPOLIA_FACTORY,
+            events: events,
+            marginAsset: marginAsset,
+            perpsV2ExchangeRate: perpsV2ExchangeRate,
+            futuresMarketManager: futuresMarketManager,
+            systemStatus: systemStatus,
+            gelato: OPTIMISM_SEPOLIA_GELATO,
+            ops: OPTIMISM_SEPOLIA_OPS,
+            settings: settings,
+            universalRouter: OPTIMISM_SEPOLIA_UNISWAP_UNIVERSAL_ROUTER,
+            permit2: OPTIMISM_SEPOLIA_UNISWAP_PERMIT2
         });
 
         implementation = address(new Account(params));
